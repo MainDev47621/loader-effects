33bdb032abf977e62b696907a3d20e7e5b292767
WebDev47621 <webdev47621@gmail.com>
2021-07-20 16:49:28 -0700

    Add homepage and loader html and css pages

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const source = document.getElementById('loader-options');
+const source = document.getElementById('loader-options').innerHTML;
 const template = Handlebars.compile(source);
+const loaderNames = ['circles-wave', 'dot-circle', 'dots-hue-rotate', 'glass-circle', 'gradient-spin', 'nested-circles', 'text-fill'];
+const loaderObjects = loaderNames.map(loader => (
+    {
+        name: loader,
+        url: `./loaders-html/${loader}.html`
+    }
+));
+console.log(loaderObjects);
 const context = {
-    loaders: []
+    loaders: loaderObjects
 };
 const compiledHtml = template(context);
+const optionsDisp = document.getElementById('options');
+optionsDisp.innerHTML = compiledHtml;
